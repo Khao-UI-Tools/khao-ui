effcd5b8f3c48f9bdbe5a8485396dc8f127eb7ec
Jens Ihrig <jens.ihrig@mail.de>
2024-08-16 23:50:27 +0200

    updated storybook

---
+++ storybook-static/assets/IconButton.stories-ixX_MHsr.js
@@ -0,0 +1,31 @@
+import"./IconButton-DaxWXB1Y.js";import"./index-CCgjxTXL.js";import"./Icon-D7rHeijA.js";const f={title:"Buttons/IconButton",component:"khao-icon-button",tags:["autodocs"],argTypes:{customBGColor:{control:"color"},customColor:{control:"color"},priority:{control:{type:"select"},options:["primary","secondary","tertiary"]},iconName:{control:{type:"select"},options:["cart","external-link","facebook","instagram","newsletter","pinterest","printer","rss"]},title:{control:"text",type:"string"},href:{control:"text",type:"string"},rel:{control:"text",type:"string"},target:{control:"text",type:"string"},onClick:{type:"function"}}},r={args:{title:"Primary with Printer",priority:"primary",iconName:"printer"}},t={args:{title:"Secondary with Cart",priority:"secondary",iconName:"cart"}},o={args:{title:"Tertiary with Newsletter",priority:"tertiary",iconName:"newsletter"}},e={args:{iconName:"facebook",customBGColor:"#3b5998",customColor:"white"}},a={args:{iconName:"pinterest",priority:"secondary",customHoverColor:"#bd081c"}};var s,n,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: "Primary with Printer",
+    priority: "primary",
+    iconName: "printer"
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var i,m,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    title: "Secondary with Cart",
+    priority: "secondary",
+    iconName: "cart"
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var l,y,u;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    title: "Tertiary with Newsletter",
+    priority: "tertiary",
+    iconName: "newsletter"
+  }
+}`,...(u=(y=o.parameters)==null?void 0:y.docs)==null?void 0:u.source}}};var d,g,C;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    iconName: "facebook",
+    customBGColor: "#3b5998",
+    customColor: "white"
+  }
+}`,...(C=(g=e.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var w,N,x;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    iconName: "pinterest",
+    priority: "secondary",
+    customHoverColor: "#bd081c"
+  }
+}`,...(x=(N=a.parameters)==null?void 0:N.docs)==null?void 0:x.source}}};const k=["Primary","Secondary","Tertiary","CustomColorsExample","CustomHoverColorExample"];export{e as CustomColorsExample,a as CustomHoverColorExample,r as Primary,t as Secondary,o as Tertiary,k as __namedExportsOrder,f as default};
