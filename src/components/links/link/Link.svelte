bcae19607f61c98f30498bf6165608da626dc781
Jens Ihrig <jens.ihrig@mail.de>
2024-08-21 08:14:59 +0200

    improve link component

--- src/components/links/link/Link.svelte
+++ src/components/links/link/Link.svelte
@@ -1,43 +1,56 @@
 <svelte:options customElement="khao-link" />
 
 <script lang="ts">
-   import Icon from "../../../icons/Icon.svelte";
-   import { type IconName } from "../../../icons/types/IconName";
+    import Icon from "../../../icons/Icon.svelte";
+    import { type IconName } from "../../../icons/types/IconName";
+    import { type IconLocation, iconLocationDefault } from "./LinkIconLocation";
 
-   export let label: string;
-   export let title: string = label;
+   
+    export let title: string = "";
 
-  export let href: string | null = null;
-  export let rel: string | null = null;
-  export let target: string | null = null;
-  export let iconName: IconName | "" = "";
+    export let href: string | null = null;
+    export let rel: string | null = null;
+    export let target: string | null = null;
+    export let iconName: IconName | "" = "";
+    export let iconLocation: IconLocation = iconLocationDefault;
 
 </script>
 
-<a class="link" href={href} title={title} rel={rel} target={target}>
-    <span class="label">{label}</span>
+<a class="link {iconName !== "" ? 'link-with-icon'  : ''} icon-location-{iconLocation}" href={href} title={title} rel={rel} target={target}>
+    <slot>Empty Link</slot>
     {#if iconName !== ""}
-    <Icon iconName={iconName} sizeFactor="4" opacity="80%"/>
+    <span class="icon" aria-hidden="true">
+        <Icon iconName={iconName} sizeFactor="4" opacity="80%"/>
+    </span>
     {/if}
 </a>
-
 
 <style>
 
     :host {
         --khao-link-icon-size: var(--khao-sys-size-regular-4);
         --khao-link-icon-space: var(--khao-sys-size-regular-1);
+        --khao-link-space-to-next-char: 0.2rem;
     }
 
-
     .link {
-        display: inline-flex;
         color: currentColor;
+        word-wrap: break-word;
         text-decoration: underline;
         text-underline-offset: var(--khao-sys-size-regular-1);
-        gap: var(--khao-link-icon-space);
         cursor: pointer;
+        vertical-align: baseline;
+    }
+
+    .link-with-icon {
+        display: inline-flex;
+        flex-direction: row;
         align-items: center;
+        gap: var(--khao-link-icon-space);
+    }
+
+    .icon-location-before {
+        flex-direction: row-reverse;
     }
 
     .link:hover {
@@ -48,10 +61,8 @@
         text-decoration-thickness: 2px;
     }
 
-    .label {
-        display: inline-block;
-        vertical-align: center;
-        line-height: var(--khao-link-icon-size);
+    .icon {
+        margin: 0 var(--khao-link-space-to-next-char) var(--khao-sys-size-regular-1) 0;
     }
 
 
---
+++ src/components/links/link/LinkIconLocation.ts
@@ -0,0 +1,5 @@
+export const iconLocations = ["before", "after"];
+
+export type IconLocation = (typeof iconLocations)[number];
+
+export const iconLocationDefault: IconLocation = "after";
