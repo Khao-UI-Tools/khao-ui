290689cb4e05889d071844d65a98cef32ca4670a
Jens Ihrig <jens.ihrig@mail.de>
2024-10-22 22:21:40 +0200

    upgraded storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
 
   framework: {
