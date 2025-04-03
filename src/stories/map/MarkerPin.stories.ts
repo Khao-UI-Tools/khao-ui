a802688ee5ca34aacf24bc73b3443f0730ba0e00
Jens Ihrig <jens.ihrig@mail.de>
2025-04-03 08:01:58 +0200

    moved all stories to the stories folder

---
+++ src/stories/map/MarkerPin.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/web-components-vite";
+import MarkerPin from "../../components/map/markerPin/MarkerPin.svelte";
+import { iconNames } from "../../icons/types/IconName";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: "maps/MarkerPin",
+  component: "khao-map-marker-pin",
+  tags: ["autodocs"],
+  argTypes: {
+    color: { control: "color" },
+    iconName: {
+      control: { type: "select" },
+      options: iconNames,
+    },
+  },
+} satisfies Meta<MarkerPin>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Restaurant: Story = {
+  args: {
+    color: "#ff9900",
+    iconName: "restaurant",
+  },
+};
+
+export const Cart: Story = {
+  args: {
+    color: "#249b57",
+    iconName: "cart",
+  },
+};
+
+export const Closed: Story = {
+  args: {
+    color: "#cccccc",
+    iconName: "close",
+  },
+};
