678101a10d6a20c171f7e9ffaf9e0fa440ff7516
Jens Ihrig <jens.ihrig@mail.de>
2025-04-07 14:06:38 +0200

    deploy storybook to subdirectory

--- packages/khao-ui/.storybook/main.ts
+++ packages/khao-ui/.storybook/main.ts
@@ -2,23 +2,25 @@ import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
   addons: [
-    '@storybook/addon-svelte-csf',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    "@storybook/addon-svelte-csf",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
   ],
   docs: {
     autodocs: true,
   },
   framework: {
-    name: '@storybook/web-components-vite',
+    name: "@storybook/web-components-vite",
     options: {},
   },
-  staticDirs: ['../storybook-assets'],
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|ts|svelte)',
-  ],
+  staticDirs: ["../storybook-assets"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|ts|svelte)"],
+  viteFinal(config, { configType }) {
+    config.base = process.env.BASE_PATH || config.base;
+    return config;
+  },
 };
+
 export default config;
